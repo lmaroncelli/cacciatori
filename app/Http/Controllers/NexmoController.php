e9ecb2e01fa79bd8afd0e2c752e2a2c571775495
luigi <lmaroncelli@gmail.com>
2019-12-19 17:22:51 +0100

    Nexmo webhook

--- app/Http/Controllers/NexmoController.php
+++ app/Http/Controllers/NexmoController.php
@@ -3,14 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class NexmoController extends Controller
 {   
 
     # https://developer.nexmo.com/concepts/guides/webhooks
-
+    # https://developer.nexmo.com/messaging/sms/guides/inbound-sms
     public function reply(Request $request)
       {
+      $str = http_build_query($request->all(),'',', ');
       
+      Log::channel('sms_log')->info('request = '.$str);
       }
 }
